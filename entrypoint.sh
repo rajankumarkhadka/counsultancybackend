2af8b34467f0c7d3f2edeb2c2fecec861b71049b
Suresh Chand <scthakuri12a@gmail.com>
2026-02-27 14:39:21 +0545

    Refactor entrypoint to use exec for command execution
    Replace gunicorn command with exec for passing arguments.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 set -e
+
 until nc -z $DB_HOST $DB_PORT; do
   sleep 1
 done
+
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-exec gunicorn counsultancybackend.wsgi:application \
-    --bind 0.0.0.0:8000 \
-    --workers 4 \
-    --timeout 120 \
-    --access-logfile - \
-    --error-logfile -
+
+exec "$@"
